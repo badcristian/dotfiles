3874d85d9000d507ef6effa9f8d94120830cc3e3
badcristian <67512817+badcristian@users.noreply.github.com>
2026-02-03 21:17:25 +0200

    alter tmux starter

--- files_to_symlink/init_tmux_sessions.sh
+++ files_to_symlink/init_tmux_sessions.sh
@@ -5,8 +5,8 @@ tmux new-session -d -s ~ -c ~/
 
 # ribeit-api
 tmux new-session -d -s ribeit-api -c /Users/mac/dev/ribeit-api
-tmux split-window -v -t ribeit-api -c /Users/mac/dev/ribeit-api
-tmux split-window -v -t ribeit-api -c /Users/mac/dev/ribeit-api
+tmux split-window -h -t ribeit-api -c /Users/mac/dev/ribeit-api
+tmux split-window -v -t ribeit-api:1.1 -c /Users/mac/dev/ribeit-api
 tmux select-pane -t ribeit-api:1.0
 
 # public-api
@@ -15,7 +15,7 @@ tmux new-session -d -s dfs-api -c /Users/mac/dev/dfs-api
 
 # ribeit-ui
 tmux new-session -d -s ribeit-ui -c /Users/mac/dev/ribeit-ui
-tmux split-window -v -t ribeit-ui -c /Users/mac/dev/ribeit-ui
+tmux split-window -h -t ribeit-ui -c /Users/mac/dev/ribeit-ui
 tmux select-pane -t ribeit-ui:1.0
 
 # usb-token-client
@@ -23,21 +23,21 @@ tmux new-session -d -s usb-token-client -c /Users/mac/dev/usb-token-client
 
 # spro-app
 tmux new-session -d -s spro-app -c /Users/mac/dev/spro-app
-tmux split-window -v -t spro-app -c /Users/mac/dev/spro-app
-tmux split-window -v -t spro-app -c /Users/mac/dev/spro-app
+tmux split-window -h -t spro-app -c /Users/mac/dev/spro-app
+tmux split-window -v -t spro-app:1.1 -c /Users/mac/dev/spro-app
 tmux send-keys -t spro-app:1.1 "npm run watch" # Command ready but not executed
 tmux send-keys -t spro-app:1.2 "redis-server" # Command ready but not executed
 tmux select-pane -t spro-app:1.0
 
 # spro-wordpress
 tmux new-session -d -s spro-wordpress -c /Users/mac/dev/spro-wordpress
-tmux split-window -v -t spro-wordpress -c /Users/mac/dev/spro-wordpress
+tmux split-window -h -t spro-wordpress -c /Users/mac/dev/spro-wordpress
 tmux select-pane -t spro-wordpress:1.0
 
 # spro-marketing
 tmux new-session -d -s spro-marketing -c /Users/mac/dev/spro-marketing
-tmux split-window -v -t spro-marketing -c /Users/mac/dev/spro-marketing
-tmux split-window -v -t spro-marketing -c /Users/mac/dev/spro-marketing
+tmux split-window -h -t spro-marketing -c /Users/mac/dev/spro-marketing
+tmux split-window -v -t spro-marketing:1.1 -c /Users/mac/dev/spro-marketing
 tmux select-pane -t spro-marketing:1.0
 
 # Attach to first session
